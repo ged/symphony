f0de59f1f7f22ec8011b1476e9db2948ddd08913
Michael Granger <ged@FaerieMUD.org>
2014-03-12 17:08:03 -0700

    Checkpoint commit of AMQP work

--- lib/groundcontrol/tasks/auditor.rb
+++ lib/groundcontrol/tasks/auditor.rb
@@ -11,11 +11,17 @@ class Auditor < GroundControl::Task
 	# Audit all events
 	subscribe_to '#'
 
+	#acknowledge false
+	prefetch 1000
+
+	queue_name 'foomanchoo'
+
 	### Create a new Pinger task for the given +job+ and +queue+.
 	def initialize( queue )
 		super
 		@logdir = Pathname( Dir.tmpdir )
 		@logfile = @logdir + 'events.log'
+		$stderr.puts "Logfile is: %s" % [ @logfile ]
 		@log = @logfile.open( File::CREAT|File::APPEND|File::WRONLY, encoding: 'utf-8' )
 	end
 
@@ -30,10 +36,20 @@ class Auditor < GroundControl::Task
 
 	# Do the ping.
 	def work( payload, metadata )
-		msg = "%s %s" % [ metadata, payload ]
-		puts( payload.inspect )
-		@log.puts( msg )
 		return true
+		val = Random.rand
+		puts( payload.inspect )
+
+		case
+		when val < 0.33
+			raise "OOOOOPS! %p" % [ payload['key'] ]
+		when val < 0.66
+			return false
+		else
+			@log.puts( payload['key'] )
+			@log.flush
+			return true
+		end
 	end
 
 
