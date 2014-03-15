0c950fa2d2269c09a796dd4a9f6d67a8e240e761
Michael Granger <ged@FaerieMUD.org>
2014-03-14 20:53:08 -0700

    Fix routing mixin

--- lib/groundcontrol/routing.rb
+++ lib/groundcontrol/routing.rb
@@ -1,66 +1,77 @@
 # -*- ruby -*-
 #encoding: utf-8
 
+require 'loggability'
+
 require 'groundcontrol' unless defined?( GroundControl )
 require 'groundcontrol/mixins'
 
 
 # A mixin for adding handlers for multiple topic keys to a Task.
 module GroundControl::Routing
+	extend Loggability
+	log_to :groundcontrol
 
 	### Add some instance data to inheriting +subclass+es.
 	def self::included( mod )
+		self.log.info "Adding routing to %p" % [ mod ]
 		super
 		mod.extend( GroundControl::MethodUtilities )
+		mod.extend( GroundControl::Routing::ClassMethods )
 		mod.singleton_attr_accessor( :routes )
 		mod.routes = Hash.new {|h,k| h[k] = [] }
 	end
 
 
-	### Register an event pattern and a block to execute when an event
-	### matching that pattern is received.
-	def self::on( pattern, &block )
-		methodobj = self.make_handler_method( pattern, &block )
-		self.routing_keys << pattern
+	# Methods to add to including classes
+	module ClassMethods
 
-		pattern_re = self.make_routing_pattern( pattern )
-		self.routes[ pattern_re ] << methodobj
-	end
+		### Register an event pattern and a block to execute when an event
+		### matching that pattern is received.
+		def on( pattern, &block )
+			methodobj = self.make_handler_method( pattern, &block )
+			self.routing_keys << pattern
 
-
-	### Install the given +block+ as an instance method of the receiver, using
-	### the given +pattern+ to derive the name, and return it as an UnboundMethod
-	### object.
-	def self::make_handler_method( pattern, &block )
-		methname = self.make_handler_method_name( pattern, block )
-		self.log.info "Setting up #%s as a handler for %s" % [ methname, pattern ]
-		define_method( methname, &block )
-		return self.instance_method( methname )
-	end
+			pattern_re = self.make_routing_pattern( pattern )
+			self.routes[ pattern_re ] << methodobj
+		end
 
 
-	### Return the name of the method that the given +block+ should be installed
-	### as, derived from the specified +pattern+.
-	def self::make_handler_method_name( pattern, block )
-		_, line = block.source_location
-		pattern = pattern.
-			gsub( /#/, 'hash' ).
-			gsub( /\*/, 'star' ).
-			gsub( /\./, '_' )
-
-		return "on_%s_%d" % [ pattern, line ]
-	end
+		### Install the given +block+ as an instance method of the receiver, using
+		### the given +pattern+ to derive the name, and return it as an UnboundMethod
+		### object.
+		def make_handler_method( pattern, &block )
+			methname = self.make_handler_method_name( pattern, block )
+			self.log.info "Setting up #%s as a handler for %s" % [ methname, pattern ]
+			define_method( methname, &block )
+			return self.instance_method( methname )
+		end
 
 
-	### Return a regular expression that will match messages matching the given
-	### +routing_key+.
-	def self::make_routing_pattern( routing_key )
-		re_string = routing_key.gsub( /\./, '\\.' )
-		re_string = re_string.gsub( /\*/, '([^\.]*)' )
-		re_string = re_string.gsub( /#/, '(.*)' )
+		### Return the name of the method that the given +block+ should be installed
+		### as, derived from the specified +pattern+.
+		def make_handler_method_name( pattern, block )
+			_, line = block.source_location
+			pattern = pattern.
+				gsub( /#/, 'hash' ).
+				gsub( /\*/, 'star' ).
+				gsub( /\./, '_' )
 
-		return Regexp.compile( re_string )
-	end
+			return "on_%s_%d" % [ pattern, line ]
+		end
+
+
+		### Return a regular expression that will match messages matching the given
+		### +routing_key+.
+		def make_routing_pattern( routing_key )
+			re_string = routing_key.gsub( /\./, '\\.' )
+			re_string = re_string.gsub( /\*/, '([^\.]*)' )
+			re_string = re_string.gsub( /#/, '(.*)' )
+
+			return Regexp.compile( re_string )
+		end
+
+	end # module ClassMethods
 
 
 	### Route the work based on the blocks registered with 'on'.
@@ -81,3 +92,5 @@ module GroundControl::Routing
 
 
 end # module GroundControl::Routing
+
+
